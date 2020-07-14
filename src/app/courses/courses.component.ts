e6971b142cac8c120eea70d6c59468a22b0a0322
krystal chen <krystal.chen@pandarg.com>
2020-07-14 15:47:43 -0700

    ngForm and Submit button

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -11,6 +11,7 @@ export class CoursesComponent implements OnInit {
   // STEP 02: Add event handler to select course
   // STEP 03: Display raw json of selected course
 
+  selectCourse = null;
   courses = [
     {
       id: 1,
@@ -27,10 +28,42 @@ export class CoursesComponent implements OnInit {
       favorite: true
     }
   ];
-
+  // logic inside the constructor will be immediately fired the logic
   constructor() { }
-
+  // put initialization code for the component into lifecycle hooks
   ngOnInit(): void {
+    // setting the emptyCourse to initialize it
+   this.resetSelectedCourse();
   }
 
+  courseSelection(clickCourse){
+    this.selectCourse = clickCourse;
+    console.log('select item', this.selectCourse);
+  }
+
+  deleteItem(currentCourseId){
+    // console.log(currentCourseId);
+    if (currentCourseId === this.selectCourse){
+      console.log('delete id', currentCourseId);
+    }
+    return;
+  }
+  saveCourse(){
+   console.log("save course from submit");
+  }
+
+  resetSelectedCourse(){
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false
+    };
+    // ngModel is initialized it to an empty component in ngOnInit
+    this.selectCourse = emptyCourse;
+  }
+  cancel(){
+    this.resetSelectedCourse();
+  }
 }
